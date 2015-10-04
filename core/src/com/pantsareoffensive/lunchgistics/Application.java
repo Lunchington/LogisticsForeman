7945a861cf30725c49dedcbf817f29ed9196773b
vonselv <vonselv@gmail.com>
2015-10-04 11:00:04 -0400

    test

--- core/src/com/pantsareoffensive/lunchgistics/Application.java
+++ core/src/com/pantsareoffensive/lunchgistics/Application.java
@@ -22,7 +22,7 @@ public class Application extends Game {
 	public static boolean DEV_MODE = false;
 
     public static  final String TITLE = "Lunchgistics";
-    public  static final String VERSION ="1.0 pre alpha";
+    public  static final String VERSION ="0.01 pre alpha";
     public static final int WIDTH = 1024;
     public static final int HEIGHT = 768;
 
