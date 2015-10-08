1affccc18cb296875534e973ad2f89727e89f74d
vonselv <vonselv@gmail.com>
2015-10-08 19:53:14 -0400

    Texture Packer added
    items and worker spritesheets created.

--- desktop/src/com/pantsareoffensive/lunchgistics/desktop/DesktopLauncher.java
+++ desktop/src/com/pantsareoffensive/lunchgistics/desktop/DesktopLauncher.java
@@ -5,8 +5,6 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.pantsareoffensive.lunchgistics.Global;
 import com.pantsareoffensive.lunchgistics.LogisticsForeman;
 
-import javax.xml.bind.annotation.XmlElementDecl;
-
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
@@ -17,9 +15,9 @@ public class DesktopLauncher {
 		config.height = Global.HEIGHT;
 
 
-		//TexturePacker.Settings settings = new TexturePacker.Settings();
-		//TexturePacker.process(settings, "src/tiles", "tiles", "tiles");
-		//TexturePacker.process(settings, "src/gui", "gui", "gui");
+		//PackTextures.Settings settings = new PackTextures.Settings();
+		//PackTextures.process(settings, "src/tiles", "tiles", "tiles");
+		//PackTextures.process(settings, "src/gui", "gui", "gui");
 
 		new LwjglApplication(new LogisticsForeman(), config);
 	}
