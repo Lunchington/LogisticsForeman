688ce2ff70e335ed2ef5f56d77836b5c9229d04b
vonselv <vonselv@gmail.com>
2015-10-18 20:37:59 -0400

    zooming works!

--- core/src/com/pantsareoffensive/lunchgistics/input/CameraScroll.java
+++ core/src/com/pantsareoffensive/lunchgistics/input/CameraScroll.java
@@ -3,12 +3,15 @@ package com.pantsareoffensive.lunchgistics.input;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 public class CameraScroll extends InputAdapter{
     private OrthographicCamera camera;
     private Vector2 move = new Vector2(0,0);
     private float scrollSpeed = 500f;
+
+    public float zoomSpeed = 0.2f;
 
     public CameraScroll(OrthographicCamera camera) { this.camera = camera; }
 
@@ -54,20 +57,20 @@ public class CameraScroll extends InputAdapter{
                 move.add(0, 1);
                 return true;
 
-            case Input.Keys.Q:
-                camera.zoom += 1;
-                return true;
-
-            case Input.Keys.E:
-                camera.zoom -= 1;
-                return true;
-
             default:
                 return false;
         }
     }
+    @Override
+    public boolean scrolled(int amount) {
+        if(amount>0) camera.zoom += zoomSpeed;
+        if(amount<0) camera.zoom -= zoomSpeed;
+        return false;
+    }
+
 
     public void update(float delta) {
+        camera.zoom = MathUtils.clamp(camera.zoom,0.4f,3f);
         Vector2 movement = move.cpy().nor().scl(scrollSpeed * delta);
         camera.translate(movement.x , movement.y, 0);
         camera.update();
