8d214a8c0dfc25ee1f82c2b8d77798252f5342aa
Lunchington <lordlunchington@gmail.com>
2015-12-12 13:07:40 -0500

    tst

--- core/src/com/pantsareoffensive/lunchgistics/Global.java
+++ core/src/com/pantsareoffensive/lunchgistics/Global.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class Global {
     public static final String TITLE = "Logistics Foreman";
-    public static final String VERSION ="0.2 pre alpha";
+    public static final String VERSION ="0.02 pre alpha";
     public static final int WIDTH = 1024;
     public static final int HEIGHT = 768;
 
